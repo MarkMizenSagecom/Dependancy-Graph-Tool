306689741455b759d9b9d9747355bde51b64ccc4
MarkMizenSagecom <mark.mizen@sage.com>
2021-03-31 11:57:41 +0100

    added firebase config

--- src/index.js
+++ src/index.js
@@ -8,12 +8,27 @@ import * as serviceWorker from "./serviceWorker";
 
 import GlobalStyle from "carbon-react/lib/style/global-style";
 
+import "firebase/firestore";
+import { FirebaseAppProvider } from "reactfire";
+
+/* Add your config from the Firebase Console */
+const firebaseConfig = {
+  apiKey: "AIzaSyAESY7RU998b2rdyyWMhCkt4mfQXfEdpyM",
+  authDomain: "dependancy-graph.firebaseapp.com",
+  projectId: "dependancy-graph",
+  storageBucket: "dependancy-graph.appspot.com",
+  messagingSenderId: "257602314188",
+  appId: "1:257602314188:web:b6daa829c5e07f23648769",
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
